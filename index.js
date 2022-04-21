b766ea8d7be769ec67f88b1c29ff27d4457bfbd7
Thomas <vanotterloo.thomas@gmail.com>
2022-04-21 07:06:01 +0200

    connectie server werkt.

--- index.js
+++ index.js
@@ -1,14 +1,75 @@
-const http = require("http");
 
-// const hostname = "127.0.0.1";
-const port = process.env.PORT || 3000;
+const express = require('express')
+const app = express()
+const port = process.env.PORT ||3000
+const bodyParser = require ('body-parser');
+app.use(bodyParser.json());
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/html");
-  res.end("Hello World\n");
+let database = [];
+let id = 0;
+
+app.all('*', (req,res,next)=>{
+  const method = req.method
+  console.log('Methode ${method} aangeropen');
+  next();
 });
 
-server.listen(port, () => {
-  console.log(`Server running at port ${port}`);
+app.get('/', (req, res) => {
+  res.status(200).json({
+    status: 200,
+    result: 'Hello world! Connection with the API went successful',
+  });
 });
+
+app.post('/api/movie',(req,res)=> {
+  let movie = req.body;
+  console.log('post function wordt aangeroepen.');
+  id++;
+  movie = {
+    id,
+    ...movie,
+  };
+
+  database.push(movie);
+  console.log(database);
+  res.status(201).json({
+    status: 201,
+    result: movie,
+  });
+});
+
+app.get('/api/movie',(req,res) => {
+  res.status(200).json({
+    status:200,
+    result: database,
+  })
+});
+
+// movieId is een parameter
+app.get('/api/movie/:movieId',(req,res) => {
+  const movieId = req.params.movieId
+  let movie = database.filter((item) => (item.id==movieId));
+  if(movie.length>0){
+    console.log(movie)
+    res.status(200).json({
+      status: 200,
+      result: movie,
+    });
+  } else {
+    res.status(404).json({
+      status: 404,
+      result: 'Movie with ID ${movieId} not found'
+    });
+  }
+});
+
+app.all('*', (req,res) => {
+  res.status(404).json({
+    status: 400,
+    result: 'End-point not found',
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
